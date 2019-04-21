7bf1d0ffa077f74ee256bfcadc7a38b7c7f61329
Freddie Pike <Freddie@colabsoftware.com>
2019-04-20 22:39:10 -0230

    Implementing iterative fibonacci sequence in Rust.

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(message: &str) {
+    let test = "cool";
+    alert(test);
+}
+
+#[wasm_bindgen]
+pub fn fibo(n: usize) -> Option<usize> {
+  let mut x = vec![1, 1];
+  for i in 2..n {
+      let next_x = x[i - 1] + x[i - 2];
+      x.push(next_x)
+  }
+  return x.last().cloned();
+}
