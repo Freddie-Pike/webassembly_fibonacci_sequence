9769e066166f23f1c017f5af5cee0d7cae1c4343
Freddie Pike <Freddie@colabsoftware.com>
2019-04-24 14:44:56 -0230

    Changing assembly script recursive fib to a switch style.

--- assembly/index.ts
+++ assembly/index.ts
@@ -2,9 +2,15 @@
 
 // Recursive version of the fibonacci sequence.
 export function wasmRecursiveFib(n: i32): i32 {
-  if (n <= 1) {
-    return n;
-  } else {
-    return (wasmRecursiveFib(n - 1) + wasmRecursiveFib(n - 2));
+  switch (n) {
+    case 0:
+      return 1;
+      break;
+    case 1:
+      return 1;
+      break;
+    default:
+      return (wasmRecursiveFib(n - 1) + wasmRecursiveFib(n - 2));
+      break;
   }
 }
